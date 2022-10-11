b41581db0b950a1f8c64342b8d6ef8f2a1ea959f
Arnor Ameti <arnameti@gmail.com>
2022-10-11 16:08:29 +0200

    JS Setup - Computer Selection

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const playerContainer = document.querySelector('.player-container');
 const computerContainer = document.querySelector('.computer-container');
 const playerIcons = document.querySelectorAll('.player-icon');
 const computerIcons = document.querySelectorAll('.computer-icon');
+const playerChoice = document.querySelector('.player-choice');
+const computerChoice = document.querySelector('.computer-choice');
 
 
 const choices = {
@@ -13,13 +15,29 @@ const choices = {
   spock: {name: 'Spock', defeats: ['scissors', 'rock']},
 };
 
+const getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 
 playerContainer.addEventListener('click', function (e) {
+  // player selection
   const clicked = e.target.closest('.player-icon');
 
   if (!clicked) return;
 
+  // setting color and name for player selection
   playerIcons.forEach((icon) => icon.classList.remove('color-black'));
   clicked.classList.add('color-black');
+  playerChoice.textContent = ` --- ${clicked.dataset.choice}`;
+
+  // computer selection
+  const randomInt = getRandomInt(0, 4);
+  const computerSelection = computerIcons[randomInt];
+
+  // setting color and name for computer selection
+  computerIcons.forEach((icon) => icon.classList.remove('color-black'));
+  computerSelection.classList.add('color-black');
+  computerChoice.textContent = ` --- ${computerSelection.dataset.choice}`
 });
 
